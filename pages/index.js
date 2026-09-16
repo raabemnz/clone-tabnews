89e34b42fc3e778d7b0762075ff7f8618ec1d479
raabemnz <raabemnz@gmail.com>
2026-09-16 01:17:49 +0000

    adicionado scripts 'lint:check' e 'lint:fix'

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1> ☺ Vamos jogar LOST? Estamos esperando você! :P☺
-</h1>
+  return <h1> ☺ Vamos jogar LOST? Estamos esperando você! :P ☺</h1>;
 }
 
 export default Home;
